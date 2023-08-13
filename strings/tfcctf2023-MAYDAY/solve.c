c04b1abc2b9c88cf95fb630cc36349a6f7d9b650
Mehdi Sahraeei <76227011+mehdisahraeei@users.noreply.github.com>
2023-08-13 07:20:25 +0330

    Update solve.c
    commentation in code for better understanding

--- strings/tfcctf2023-MAYDAY/solve.c
+++ strings/tfcctf2023-MAYDAY/solve.c
@@ -2,17 +2,19 @@
 #include <string.h>
 #include <stdbool.h>
 
-
-// Why 6? Longest specials: Three+\0, Seven+\0, Eight+\0
+// The maximum length of special token keys, chosen based on the longest keys: Three, Seven, Eight
 #define SPECIALS_MAX_KEY 6UL
 
+// Define a structure to encapsulate special tokens and their corresponding values
 typedef struct {
-    const char k[SPECIALS_MAX_KEY];
-    char v;
+    const char k[SPECIALS_MAX_KEY]; // Key representing a special token
+    char v;                         // Corresponding value for the special token
 } specials_t;
 
+// Determine the size of the specials array based on the number of elements
 #define SPECIALS_SIZE (sizeof(specials) / sizeof(specials[0]))
 
+// An array of special tokens and their associated values
 const specials_t specials[] = {
     { .k = "One",   .v = '1' },
     { .k = "Two",   .v = '2' },
@@ -27,42 +29,45 @@ const specials_t specials[] = {
     { .k = "Dash",  .v = '-' }
 };
 
-
+// Delimiter employed to tokenize the input response
 #define DELIMITER " "
 
-
+// Function dedicated to discerning a special token and extracting its corresponding value
 static inline bool find_special_token(const char* token, char* output) {
     for (size_t i = 0; i < SPECIALS_SIZE; i++) {
+        // Check if the current token matches any of the defined special token keys
         if (strncmp(token, specials[i].k, SPECIALS_MAX_KEY) == 0) {
-            *output = specials[i].v;
-            return true;
+            *output = specials[i].v; // Assign the associated value to the output
+            return true;             // Indicate successful identification of a special token
         }
     }
-    return false;
+    return false; // No special token found
 }
 
-
-// ----<main>------------------------------------------------------------------
-
+// Main execution point
 int main(void)
 {
+    // Input response comprising a fusion of special tokens and conventional words
     char response[] = "Whiskey Hotel Four Tango Dash Alpha Romeo Three Dash"
                       " Yankee Oscar Uniform Dash Sierra One November Kilo"
                       " India November Golf Dash Four Bravo Zero Uniform Seven";
 
+    // Tokenize the input response using the designated delimiter
     char* token = strtok(response, DELIMITER);
     while (token != NULL) {
-        char special_token = { 0 };
+        char special_token = { 0 }; // Placeholder for the special token's value
 
+        // Identify if the current token corresponds to a special token, if so, print its value
         if (find_special_token(token, &special_token)) {
             putchar(special_token);
         } else {
+            // In the absence of a special token match, print the initial character of the token
             putchar(token[0]);
         }
 
-        token = strtok(NULL, DELIMITER);
+        token = strtok(NULL, DELIMITER); // Move to the next token
     }
-    putchar('\n');
+    putchar('\n'); // Print a newline character to finalize the output
 
-    return 0;
+    return 0; // Indicate successful program execution
 }
